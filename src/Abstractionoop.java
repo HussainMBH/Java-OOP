3335f6180adb9c3742db2096c00c4035f01c085f
Mohamed Bahir Hussain <mbahirh624@gmail.com>
2023-08-29 23:10:30 +0530

    Abstraction

---
+++ src/Abstractionoop.java
@@ -0,0 +1,12 @@
+//Abstraction = Hiding the Unnecessary parts and showing the necessary parts
+// 2 type: Abstraction Class and Abstraction Methods
+
+abstract class Abst{
+    abstract void Abstra();
+}
+
+public class Abstractionoop {
+    public static void main(String[] args){
+
+    }
+}
